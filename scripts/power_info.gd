98c8e0bd78bb69e728349f611308d54f4f545da8
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-09-11 15:42:42 -0500

    Polished UI Systems

--- scripts/power_info.gd
+++ scripts/power_info.gd
@@ -3,7 +3,7 @@ extends Popup
 
 
 func _on_show_power_info(power: Power):
-	$VBox/NameContainer/Name.text = power.name
+	$VBox/Name.text = power.name
 	$VBox/ScrollContainer/VBox/Level.text = "[b]Level:[/b] %s" % str(power.level)
 	$VBox/ScrollContainer/VBox/Period.text = "[b]Casting Period:[/b] %s" % power.castingPeriodValues.keys()[power.castingPeriod]
 	$VBox/ScrollContainer/VBox/Duration.text = "[b]Duration:[/b] %s" % power.duration
