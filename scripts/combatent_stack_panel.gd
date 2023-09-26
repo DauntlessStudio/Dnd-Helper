3e74a26454dc3157d9b3c03998440fa52ca1caf0
DauntlessStudio <42518863+DauntlessStudio@users.noreply.github.com>
2023-09-25 20:52:09 -0500

    Enhanced Usability

--- scripts/combatent_stack_panel.gd
+++ scripts/combatent_stack_panel.gd
@@ -36,8 +36,7 @@ func set_player(playerName: String, init: int, armor: String, movement: String, 
 	$VBoxContainer/HBoxContainer/VBoxContainer/HBoxContainer/ArmorLabel.text = " %s" % armor
 
 func _on_hp_slider_value_changed(value):
-	currentHP = value
-	$VBoxContainer/HBoxContainer/VBoxContainer/HPLabel.text = " %d/%d" % [currentHP, maximumHP]
+	hp_value_changed(value)
 
 
 func _on_clear_button_pressed():
@@ -47,3 +46,31 @@ func _on_clear_button_pressed():
 func _on_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		combatantInfo.emit(self)
+
+
+func _on_hp_label_pressed():
+	$Window/Panel/SpinBox.value = 0
+	$Window.popup()
+	$Window/Panel/SpinBox.get_line_edit().grab_focus()
+
+
+func _on_add_button_pressed():
+	var value = clampi(currentHP + int($Window/Panel/SpinBox.value), 0, maximumHP)
+	hp_value_changed(value)
+	_on_window_close_requested()
+
+
+func _on_subtract_button_pressed():
+	var value = clampi(currentHP - int($Window/Panel/SpinBox.value), 0, maximumHP)
+	hp_value_changed(value)
+	_on_window_close_requested()
+	
+func hp_value_changed(value):
+		currentHP = value
+		$VBoxContainer/HBoxContainer/VBoxContainer/HPLabel.text = " %d/%d" % [currentHP, maximumHP]
+		$VBoxContainer/HBoxContainer/VBoxContainer/HSlider.value = value
+
+
+func _on_window_close_requested():
+	$Window.hide()
+
